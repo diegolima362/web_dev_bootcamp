d53c94f8bea0616fbf15c6a85f668f19e5db4aa0
diego <diegolima362@gmail.com>
2020-05-07 20:26:59 -0300

    end of js and dom manipulation

---
+++ 8_12_js_and_dom/drum/index.js
@@ -0,0 +1,54 @@
+var buttons = document.querySelectorAll('.drum');
+
+for (var i = 0; i < buttons.length; i++) {
+    buttons[i].addEventListener('click', function() {
+        playSound(this.innerHTML);
+        buttonAnimation(this.innerHTML);
+    });
+}
+
+document.addEventListener('keypress', function(event) {
+    playSound(event.key);
+    buttonAnimation(event.key);
+});
+
+function playSound(key) {
+    var audio;
+
+    switch (key) {
+        case 'w':
+            audio = new Audio('./sounds/snare.mp3');
+            break;
+        case 'a':
+            audio = new Audio('./sounds/tom-1.mp3');
+            break;
+        case 's':
+            audio = new Audio('./sounds/tom-3.mp3');
+            break;
+        case 'd':
+            audio = new Audio('./sounds/kick-bass.mp3');
+            break;
+        case 'j':
+            audio = new Audio('./sounds/tom-4.mp3');
+            break;
+        case 'k':
+            audio = new Audio('./sounds/tom-2.mp3');
+            break;
+        case 'l':
+            audio = new Audio('./sounds/crash.mp3');
+            break;
+        default:
+            audio = null;
+    }
+
+    if (audio) audio.play();
+}
+
+function buttonAnimation(key) {
+    var activeButton = document.querySelector('.' + key);
+
+    activeButton.classList.add('pressed');
+    setTimeout(function() {
+        activeButton.classList.remove('pressed');
+    }, 100);
+}
